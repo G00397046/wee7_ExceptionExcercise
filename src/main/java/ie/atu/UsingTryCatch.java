5686443fc49ba797de2988bc8afe39011a39906c
G00397046 <G00397046@atu.ie>
2022-11-08 13:31:39 +0000

    Setup Try/Catch Added New Branch

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
-        System.out.println("Please enter a number between 0-9 : ");
-        Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
 
-        if(number <= 9 && number > 0){
-            System.out.println("You entered " + number);
+        try{
+            System.out.println("Please enter a number between 0-9 : ");
+            Scanner input = new Scanner(System.in);
+            int number = input.nextInt();
+            System.out.println("You entered: " + number);
+
         }
-        else{
-            System.out.println("Not a valid number");
+        catch (InputMismatchException e){
+            System.out.printf("Not a valid number");
         }
+
     }
 }
